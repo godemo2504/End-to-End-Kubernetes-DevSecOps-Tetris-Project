c4f795a59c50225d37ff06ad184a1ff772a8f52a
godemo2504 <godemo2504@gmail.com>
2024-12-18 10:06:13 +0100

    Update iam-policy.tf

--- EKS-TF/iam-policy.tf
+++ EKS-TF/iam-policy.tf
@@ -1,3 +1,4 @@
+# Attachement des politiques existantes
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.EKSClusterRole.name
@@ -16,4 +17,40 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.NodeGroupRole.name
+}
+
+# Nouvelle politique pour S3 et DynamoDB
+resource "aws_iam_role_policy" "TerraformS3DynamoDBPolicy" {
+  name = "TerraformS3DynamoDBPolicy"
+  role = aws_iam_role.EKSClusterRole.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::bucket-godemo",
+          "arn:aws:s3:::bucket-godemo/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = "arn:aws:dynamodb:us-east-1:${data.aws_caller_identity.current.account_id}:table/tablegodemo1"
+      }
+    ]
+  })
 }
